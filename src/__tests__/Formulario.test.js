1abb954144ad3766a8e24836ca325043c231d6d2
Jorge Barcos <jorgebarcos88@gmail.com>
2021-04-27 11:51:00 -0300

    Llenar datos en los formularios

--- src/__tests__/Formulario.test.js
+++ src/__tests__/Formulario.test.js
@@ -2,7 +2,9 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Formulario from "../components/Formulario";
 import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
 
+// Spy function funcion que no es real pero simula
 const crearCita = jest.fn();
 
 test("<Formulario /> Cargar el formulario y revisar que todo sea correcto", () => {
@@ -35,10 +37,42 @@ test("<Formulario /> Validación de formulario", () => {
   fireEvent.click(btnSubmit);
 
   // Revisar por la alerta
-  expect(screen.getByTestId("alerta")).toBeInTheDocument();
-  expect(screen.getByTestId("alerta").textContent).toBe(
-    "Todos los campos son obligatorios"
-  );
-  expect(screen.getByTestId("alerta").tagName).toBe("P");
-  expect(screen.getByTestId("alerta").tagName).not.toBe("BUTTON");
+  const alerta = screen.getByTestId("alerta");
+  expect(alerta).toBeInTheDocument();
+  expect(alerta.textContent).toBe("Todos los campos son obligatorios");
+  expect(alerta.tagName).toBe("P");
+  expect(alerta.tagName).not.toBe("BUTTON");
+});
+
+test("<Formulario /> Validación de formulario", () => {
+  render(<Formulario crearCita={crearCita} />);
+
+  //fireEvent (antes)
+  // fireEvent.change(screen.getByTestId("mascota"), {
+  //   target: { value: "Hook" },
+  // });
+  // fireEvent.change(screen.getByTestId("propietario"), {
+  //   target: { value: "Jorge" },
+  // });
+  // Click en el botón de submit
+  // const btnSubmit = screen.getByTestId("btn-submit");
+  // fireEvent.click(btnSubmit);
+
+  userEvent.type(screen.getByTestId("mascota"), "Hook");
+  userEvent.type(screen.getByTestId("propietario"), "Jorge");
+  userEvent.type(screen.getByTestId("fecha"), "2021-04-04");
+  userEvent.type(screen.getByTestId("hora"), "11:42");
+  userEvent.type(screen.getByTestId("sintomas"), "Solo Duerme");
+
+  //  Click en el botón de submit
+  const btnSubmit = screen.getByTestId("btn-submit");
+  userEvent.click(btnSubmit);
+
+  // Revisar por la alerta
+  const alerta = screen.queryByTestId("alerta");
+  expect(alerta).not.toBeInTheDocument();
+
+  // Crear cita y comprobar que la función se haya llamado
+  expect(crearCita).toHaveBeenCalled();
+  expect(crearCita).toHaveBeenCalledTimes(1);
 });
