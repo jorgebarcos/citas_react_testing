6af2026d4b05d4b6881e34a0f2d0959c92e0e315
Jorge Barcos <jorgebarcos88@gmail.com>
2021-04-27 12:24:33 -0300

    Probando la aplicación Completa

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,1 +1,20 @@
-test("La aplicación funciona", () => {});
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+
+test("<App /> La aplicación funciona bien la primera vez", () => {
+  //   const wrapper = render(<App />);
+  //   wrapper.debug();
+  render(<App />);
+
+  expect(screen.getByText("Administrador de Pacientes")).toBeInTheDocument();
+  expect(screen.getByTestId("nombre-app").textContent).toBe(
+    "Administrador de Pacientes"
+  );
+  expect(screen.getByTestId("nombre-app").tagName).toBe("H1");
+
+  expect(screen.getByText("Crear Cita")).toBeInTheDocument();
+  expect(screen.getByText("No hay citas")).toBeInTheDocument();
+});
