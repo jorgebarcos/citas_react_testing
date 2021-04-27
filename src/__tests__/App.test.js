9901dda85987f3fb0cf90f1011e8d1a3c0d5d1b9
Jorge Barcos <jorgebarcos88@gmail.com>
2021-04-27 12:33:07 -0300

    Probar el titulo cuando hay citas o no

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -18,3 +18,29 @@ test("<App /> La aplicación funciona bien la primera vez", () => {
   expect(screen.getByText("Crear Cita")).toBeInTheDocument();
   expect(screen.getByText("No hay citas")).toBeInTheDocument();
 });
+
+test("<App /> La aplicación funciona bien la primera vez", () => {
+  render(<App />);
+
+  userEvent.type(screen.getByTestId("mascota"), "Hook");
+  userEvent.type(screen.getByTestId("propietario"), "Jorge");
+  userEvent.type(screen.getByTestId("fecha"), "2021-04-04");
+  userEvent.type(screen.getByTestId("hora"), "11:42");
+  userEvent.type(screen.getByTestId("sintomas"), "Solo Duerme");
+
+  //  Click en el botón de submit
+  const btnSubmit = screen.getByTestId("btn-submit");
+  userEvent.click(btnSubmit);
+
+  // Revisar por la alerta
+  const alerta = screen.queryByTestId("alerta");
+  expect(alerta).not.toBeInTheDocument();
+
+  // Revisar por el titulo dinamico
+  expect(screen.getByTestId("titulo-dinamico").textContent).toBe(
+    "Administra tus Citas"
+  );
+  expect(screen.getByTestId("titulo-dinamico").textContent).not.toBe(
+    "No hay citas"
+  );
+});
