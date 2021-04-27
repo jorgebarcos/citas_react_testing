430dea7d91bdd7884a659293bc0f539f839d5c95
Jorge Barcos <jorgebarcos88@gmail.com>
2021-04-27 10:11:51 -0300

    Seleccionar Elementos de Componentes

--- src/__tests__/Formulario.test.js
+++ src/__tests__/Formulario.test.js
@@ -1,1 +1,13 @@
-test("<Formulario /> Cargar el formulario y revisar que todo sea correcto", () => {});
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Formulario from "../components/Formulario";
+import "@testing-library/jest-dom/extend-expect";
+
+test("<Formulario /> Cargar el formulario y revisar que todo sea correcto", () => {
+  // Revisa que el componente se monto correctamente y revisa su estructura html
+  //   const wrapper = render(<Formulario />);
+  //   wrapper.debug();
+
+  render(<Formulario />);
+  expect(screen.getByText("Crear Cita")).toBeInTheDocument();
+});
